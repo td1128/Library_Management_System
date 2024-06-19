b1b86c6301e614c0ea608033f50535ac748a3e57
ihgazi <irfanhgazi@gmail.com>
2024-06-19 11:51:00 +0530

    FEAT: Create profile menu component - updated root css to fix overflow issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { Provider } from 'react-redux'
-import { store } from './app/store';
-import { BrowserRouter } from 'react-router-dom';
-import MuiBreadcrumbs from './common_components/breadcrumbs/MuiBreadcrumbs';
-import Routers from './routes/Routers';
+import { store } from './app/store'
+import { BrowserRouter } from 'react-router-dom'
+import MuiBreadcrumbs from './common_components/breadcrumbs/MuiBreadcrumbs'
+import ProfileMenu from './common_components/ProfileMenu'
+import Routers from './routes/Routers'
 import './App.css'
 
 function App() {
   return (
     <Provider store={store}>
-      <MuiBreadcrumbs/>
-      <BrowserRouter >
-        <Routers/>
+      <div className="flex justify-between px-2">
+        <MuiBreadcrumbs />
+        <ProfileMenu />
+      </div>
+      <BrowserRouter>
+        <Routers />
       </BrowserRouter>
     </Provider>
   )
 }
 
 export default App
-
