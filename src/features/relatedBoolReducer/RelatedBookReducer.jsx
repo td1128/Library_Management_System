4cef01b9ee152178a457b5fe4624baa9d761330d
Animesh Dhara <animeshdhara398@gmail.com>
2024-06-29 20:30:50 +0530

    Change the related book reducer's function name

--- src/features/relatedBoolReducer/RelatedBookReducer.jsx
+++ src/features/relatedBoolReducer/RelatedBookReducer.jsx
@@ -1,13 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialArray = [
-    {isbn: '978-3-16-148410-1',author:"abcd",title:"Learn C++ online",description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptate qui provident fuga mollitia voluptas molestiae magni quidem nobis dicta totam iste animi! Fuga veritatis iure earum ipsum soluta! Molestiae", dateOfPublication:"2023", publisher:"Mc Graw Hill", availability:true},
-    {isbn: '978-3-16-148410-2',author:"kafjja",title:"Data structure and algorithm",description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptate qui provident fuga mollitia voluptas molestiae magni quidem nobis dicta totam iste animi! Fuga veritatis iure earum ipsum soluta! Molestiae", dateOfPublication:"2021", publisher:"Publication publisher", availability:true},
-    {isbn: '978-3-16-148410-3',author:"eirip",title:"Data base management system",description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptate qui provident fuga mollitia voluptas molestiae magni quidem nobis dicta totam iste animi! Fuga veritatis iure earum ipsum soluta! Molestiae", dateOfPublication:"2022", publisher:"lkajfa lkjajf ", availability:true},
-    {isbn: '978-3-16-148410-4',author:"cvlf;fk",title:"Operating System",description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptate qui provident fuga mollitia voluptas molestiae magni quidem nobis dicta totam iste animi! Fuga veritatis iure earum ipsum soluta! Molestiae", dateOfPublication:"2020", publisher:"lafj oerpop", availability:true},
-    {isbn: '978-3-16-148410-5',author:"prkkklkp",title:"Computer Network",description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptate qui provident fuga mollitia voluptas molestiae magni quidem nobis dicta totam iste animi! Fuga veritatis iure earum ipsum soluta! Molestiae", dateOfPublication:"2023", publisher:"mfnlj irjdljls", availability:true}
-];
-
 // Convert array to object
 const initialState = {
     books: [],
