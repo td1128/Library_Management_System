00cbfd409e4a66d4c81ca3ad3d9b7e3cfd4d7201
Animesh Dhara <animeshdhara398@gmail.com>
2024-07-12 18:26:14 +0530

    Added nested routes

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -15,13 +15,13 @@ const Routers = () => {
         <Route path="/user/*" element={<Layout rootPath="/user" navItems={userNavItems} />}>
           <Route index element={<UserHome />} />
           <Route path="landing" element={<LandingPage />} /> 
+          <Route path="profile" element={<Profile />} />
+          <Route path="help" element={<Help />} />
           <Route path="books" >
             <Route index element={<Books />} />
             <Route path="viewdetails/:isbn" element={<ShowBookDetails type={'user'}/>} />  
             <Route path="goto-wishlist" element={<WishList/>} />  
           </Route>  
-          <Route path="profile" element={<Profile />} />
-          <Route path="help" element={<Help />} />
         </Route>
         <Route path="/admin/*" element={<Layout rootPath="/admin" navItems={adminNavItems} />}>
           <Route index element={<AdminHome />} />
