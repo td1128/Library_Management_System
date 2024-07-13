2daf70c8b976f04d5f926777a30168c0eb87ba44
Imamul5641 <imamulhasan9748@gmail.com>
2024-07-14 00:02:08 +0530

    Same

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Library_Management_System/', // Ensure this has the leading and trailing slashes
   plugins: [react()],
   resolve: {
     alias: {
@@ -18,7 +17,7 @@ export default defineConfig({
   },
   esbuild: {
     supported: {
-      'top-level-await': true // browsers can handle top-level-await features
+      'top-level-await': true //browsers can handle top-level-await features
     },
   }
 })
