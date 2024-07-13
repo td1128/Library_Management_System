0a0dff8dc539a247e91b35b9ab7d4d55db00469b
Imamul5641 <97669936+Imamul5641@users.noreply.github.com>
2024-07-13 21:33:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Library_Management_System',
   plugins: [react()],
   resolve: {
     alias: {
