641d42ce0dc6239d50d4c1a539204cd74ce094a4
Imamul Hasan <imamulhasan9748@gmail.com>
2024-07-14 14:48:05 +0530

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/Library_Management_System">
           <Routers />
         </BrowserRouter>
         {/* <Books/> */}
